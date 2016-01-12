63684cf0ee23c1c93431a53ac4ab243169dc2685
ANH <A.Herbst@camos.de>
2016-01-12 11:21:34 +0100

    refactoring   a) move argument parsing to function parseOpts   b) move default configuration to function setDefaultConfiguration   c) move configuration setting to function setConfiguration
    added -d parameter (target directory)
    added -t parameter (add testing repositories)

--- ArchRepoSync.sh
+++ ArchRepoSync.sh
@@ -59,133 +59,215 @@
 #	2013 11 23
 #		- removed debug output of NOARCHS and NOREPOS
 #
+#	2016 01 11
+#		- refactoring
+#			a) move argument parsing to function parseOpts
+#			b) move default configuration to function setDefaultConfiguration
+#			c) move configuration setting to function setConfiguration
+#		- added -t parameter (add testing repos)
+#		- added -d parameter (target dir)
 
 
-#
-# CONFIGURATION
-#
+function setDefaultConfiguration() {
+	#
+	# CONFIGURATION
+	#
+	CONFIG_MIRROR="rsync://mirrors.kernel.org/archlinux"
+	CONFIG_TARGETDIR="."
 
+	CONFIG_ACTION="sync"
 
-#mirror=rsync://distro.ibiblio.org/distros/archlinux
-#mirror=rsync://ftp-stud.hs-esslingen.de/archlinux
-mirror=rsync://mirrors.kernel.org/archlinux
+	CONFIG_REPOS="core extra community multilib"
+	CONFIG_TESTINGREPOS="community-testing gnome-unstable kde-unstable multilib-testing testing"
+	CONFIG_ARCHS="i686 x86_64"
 
-# repos="core extra community community-testing multilib testing"
-# archs="i686 x86_64"
+	CONFIG_INTEGRITY_CHECK=0
+	CONFIG_PARALLELDOWNLOADS=4
 
-repos="core extra community multilib community-testing gnome-unstable kde-unstable multilib-testing testing"
-archs="i686 x86_64"
-paralleldownloads=4
+	CONFIG_VERBOSITY=0
+	#
+	# END OF CONFIGURATION
+	#
+}
 
-#
-# END OF CONFIGURATION
-#
+function setConfiguration() {
+	CONFIG_MIRROR=""
+	CONFIG_REPOS=""
+	CONFIG_ARCHS=""
+	CONFIG_INTEGRITY_CHECK=""
+	CONFIG_PARALLELDOWNLOADS=""
+	CONFIG_TARGETDIR=""
+	CONFIG_ACTION=""
+	CONFIG_VERBOSITY=""
 
-ONLY_CHECK_CONSISTENCY=0
-INTEGRITY_CHECK=0
-VERBOSITY=0
-PRINT_USAGE=0
-ARG_ARCHS=""
-ARG_NOARCHS=0
-ARG_REPOS=""
-ARG_NOREPOS=0
+	local ARG_ARCHS=""
+	local ARG_ONLY_CHECK_CONSISTENCY=0
+	local ARG_TARGETDIR=""
+	local ARG_PRINT_USAGE=0
+	local ARG_INTEGRITY_CHECK=0
+	local ARG_MIRROR=""
+	local ARG_CONFIG_PARALLELDOWNLOADS=""
+	local ARG_REPOS=""
+	local ARG_ADD_TESTING_REPOS=""
+	local ARG_VERBOSITY=0
 
-opt=!
+	setDefaultConfiguration
+	parseOpts "$@"
+	
+	if ! [ "$ARG_ARCHS" = "" ]
+	then
+		if [ "$ARG_ARCHS" = "none" ]
+		then
+			CONFIG_ARCHS=""
+		else
+			CONFIG_ARCHS="$ARG_ARCHS"
+		fi
+	fi
 
-while ! [ "$opt" = "?" ]
-do
-	getopts a:cfhim:p:r:v opt $*
+	if ! echo $CONFIG_ARCHS | grep any && [ "$CONFIG_ARCHS" != "" ]
+	then
+		CONFIG_ARCHS="any $CONFIG_ARCHS"
+	fi
 
-	case $opt in
-		a)
-			ARG_ARCHS="$ARG_ARCHS $OPTARG"
+	if ! [ "$ARG_REPOS" = "" ]
+	then
+		if [ "$ARG_REPOS" = "none" ]
+		then
+			CONFIG_REPOS=""
+		else
+			CONFIG_REPOS="$ARG_REPOS"
+		fi
+	fi
 
-			if [ "$OPTARG" = "none" ]
-			then
-				ARG_NOARCHS=1
-				ARG_ARCHS=""
-			else
-				ARG_NOARCHS=0
-			fi;;
-		c)
-			ONLY_CHECK_CONSISTENCY=1;;
-		h)
-			PRINT_USAGE=1;;
-		i)
-			INTEGRITY_CHECK=1;;
-		m)
-			mirror=$OPTARG;;
-		p)
-			paralleldownloads=$OPTARG;;
-		r)
-			ARG_REPOS="$ARG_REPOS $OPTARG"
+	if [ "$ARG_REPOS" != "none" ]
+	then
+		if [ "$ARG_ADD_TESTING_REPOS" != "" ] && [ $ARG_ADD_TESTING_REPOS -eq 1 ]
+		then
+			CONFIG_REPOS="$CONFIG_REPOS $CONFIG_TESTINGREPOS"
+		fi
+	fi
 
-			if [ "$OPTARG" = "none" ]
-			then
-				ARG_NOREPOS=1
-				ARG_REPOS=""
-			else
-				ARG_NOREPOS=0
-			fi;;
-		v)
-			VERBOSITY=$(( $VERBOSITY + 1));;
-	esac
-done
+	if ! [ "$ARG_MIRROR" = "" ]
+	then
+		CONFIG_MIRROR=$ARG_MIRROR;
+	fi
 
-if [ $PRINT_USAGE -eq 1 ]
-then
-	echo "usage: [-chiv] [-a <arch>] [-r <repo>] [-m <mirror>] [-p <num parallel processes>]"
+	if ! [ "$ARG_CONFIG_PARALLELDOWNLOADS" = "" ] && [ $ARG_CONFIG_PARALLELDOWNLOADS -gt 0 ]
+	then
+		CONFIG_PARALLELDOWNLOADS=$ARG_CONFIG_PARALLELDOWNLOADS
+	fi
+
+	if [ $ARG_INTEGRITY_CHECK -eq 1 ]
+	then
+		CONFIG_INTEGRITY_CHECK=1
+	fi
+
+	if [ $ARG_ONLY_CHECK_CONSISTENCY -eq 1 ]
+	then
+		CONFIG_ACTION="check"
+	fi
+
+	if [ "$ARG_TARGETDIR" != "" ]
+	then
+		CONFIG_TARGETDIR="$ARG_TARGETDIR"
+	fi
+
+	if [ $ARG_PRINT_USAGE -eq 1 ]
+	then
+		CONFIG_ACTION="usage"
+	fi
+
+	if [ "$ARG_VERBOSITY" != "" ] && [ $ARG_VERBOSITY -ge 0 ]
+	then
+		CONFIG_VERBOSITY=$ARG_VERBOSITY
+	fi
+}
+
+function parseOpts() {
+	ARG_ARCHS=""
+	ARG_ONLY_CHECK_CONSISTENCY=0
+	ARG_TARGETDIR=""
+	ARG_PRINT_USAGE=0
+	ARG_INTEGRITY_CHECK=0
+	ARG_MIRROR=""
+	ARG_CONFIG_PARALLELDOWNLOADS=""
+	ARG_REPOS=""
+	ARG_ADD_TESTING_REPOS=0
+	ARG_VERBOSITY=0
+
+	local opt=!
+	local OPTARG
+
+	while ! [ "$opt" = "?" ]
+	do
+		getopts a:cd:him:p:r:tv opt $*
+
+		case $opt in
+			a)
+				ARG_ARCHS="$ARG_ARCHS $OPTARG";;
+			c)
+				ARG_ONLY_CHECK_CONSISTENCY=1;;
+			d)
+				ARG_TARGETDIR="$OPTARG";;
+			h)
+				ARG_PRINT_USAGE=1;;
+			i)
+				ARG_INTEGRITY_CHECK=1;;
+			m)
+				ARG_MIRROR=$OPTARG;;
+			p)
+				ARG_CONFIG_PARALLELDOWNLOADS=$OPTARG;;
+			r)
+				ARG_REPOS="$ARG_REPOS $OPTARG";;
+			t)
+				ARG_ADD_TESTING_REPOS=1;;
+			v)
+				ARG_VERBOSITY=$(( $ARG_VERBOSITY + 1));;
+		esac
+	done
+}
+
+function usage() {
+	echo "usage: $( basename "$0" ) [-chiv] [-a <arch>] [-r <repo>] [-m <mirror>] [-p <num parallel processes>]"
 	echo "options:"
-	echo " -a <arch>	select arch; one of {i686, x86_64}"
-	echo " -r <repo>	select repo; one of	{core, extra, community,"
-	echo "						community-testing, testing}"
-	echo "		to specify multiple archs/repos you need to add the"
-	echo "		-a/-r parameter multiple times"
+	echo " -a <arch>	select arch; i.e. one of {i686, x86_64, none}"
+	echo "			to specify multiple values you need to add the parameter multiple times"
+	echo "			default: -a i686 -a x86_86"
+	echo ""
+	echo " -r <repo>	select repo; i.e. one of {core, extra, community, none}"
+	echo "			to specify multiple values you need to add the parameter multiple times"
+	echo "			default: -r core -r extra -r community -r multilib"
+	echo ""
 	echo " -c		only check consistency"
+	echo " -d		target directory (default: \".\")"
 	echo " -h		show help"
 	echo " -i		do md5 integrity check"
 	echo " -m <mirror>	use specified mirror"
 	echo " -p <num>		use num parallel rsync instances"
+	echo " -t		add all testing repos"
 	echo " -v		verbose"
 	echo
-	echo "example:	./sync.sh -v -a i686 -r core -r extra -r community"
+	echo "example:	$( basename "$0" ) -v -a i686 -r core -r extra -r community"
 	echo "		download core,extra,community repos for i686 architecture"
 	echo
-
-	exit 0
-fi
-
-if ! [ "$ARG_ARCHS" = "" ] || [ $ARG_NOARCHS -eq 1 ]
-then
-	archs="$ARG_ARCHS"
-fi
-
-if ! echo $archs | grep any && ! [ $ARG_NOARCHS -eq 1 ]
-then
-	archs="any $archs"
-fi
-
-if ! [ "$ARG_REPOS" = "" ] || [ $ARG_NOREPOS -eq 1 ]
-then
-	repos="$ARG_REPOS"
-fi
+}
 
 log()
 {
 	verbositylvl=$1
 	shift 1
 
-	if [ $VERBOSITY -ge $verbositylvl ]
+	if [ $CONFIG_VERBOSITY -ge $verbositylvl ]
 	then
-		echo "$*" | tee -a sync.log
+		echo "$*" | tee -a "$CONFIG_TARGETDIR/sync.log"
 	else
-		echo "$*" >> sync.log
+		echo "$*" >> "$CONFIG_TARGETDIR/sync.log"
 	fi
 }
 
 errlog()
 {
-	echo "$*" >> sync.log
+	echo "$*" >> "$CONFIG_TARGETDIR/sync.log"
 	echo "$*" 1>&2
 }
 
@@ -193,12 +275,14 @@ errlog()
 
 consistencycheck()
 {
-	local repo=$1
-	local arch=$2
+	local targetdir=$1
+	local repo=$2
+	local arch=$3
+	local domd5sum=$4
 
 	if ! [ "$arch" = "any" ]
 	then
-		DBFILE=$repo/os/$arch/$repo.db.tar.gz
+		DBFILE="$targetdir/$repo/os/$arch/$repo.db.tar.gz"
 
 		if ! [ -e $DBFILE ]
 		then
@@ -206,7 +290,7 @@ consistencycheck()
 			return 1
 		fi
 
-		tar -xzOf $DBFILE --wildcards */desc | 
+		tar -xzOf "$DBFILE" --wildcards */desc | 
 		(
 			error=0
 			lastline=""
@@ -220,13 +304,13 @@ consistencycheck()
 				then
 					filename="$line"
 
-					if [ -L "$repo/os/$arch/$filename" ] && ! [ -e "$repo/os/any/$filename" ]
+					if [ -L "$targetdir/$repo/os/$arch/$filename" ] && ! [ -e "$targetdir/$repo/os/any/$filename" ]
 					then
 						errlog \(EE\) missing package: $repo/os/any/$filename
 						error=1
 					fi
 
-					if ! [ -e "$repo/os/$arch/$filename" ]
+					if ! [ -e "$targetdir/$repo/os/$arch/$filename" ]
 					then
 						errlog \(EE\) missing package: $repo/os/$arch/$filename
 						error=1
@@ -235,15 +319,15 @@ consistencycheck()
 				then
 					md5sum=$line
 
-					if [ $INTEGRITY_CHECK -eq 1 ] && ( echo "$md5sum  $repo/os/$arch/$filename" | md5sum -c --quiet; [ $? -eq 1 ] )
+					if [ $domd5sum -eq 1 ] && ( echo "$md5sum  $targetdir/$repo/os/$arch/$filename" | md5sum -c --quiet; [ $? -eq 1 ] )
 					then
 						errlog \(EE\) md5sum corruption of package: $repo/os/$arch/$filename ... deleting file
 
-						if ! [ -e "$repo/os/$arch/$filename" ] || [ -L "$repo/os/$arch/$filename" ]
+						if ! [ -e "$targetdir/$repo/os/$arch/$filename" ] || [ -L "$targetdir/$repo/os/$arch/$filename" ]
 						then
-							rm "$repo/os/any/$filename"
+							rm "$targetdir/$repo/os/any/$filename"
 						else
-							rm "$repo/os/$arch/$filename"
+							rm "$targetdir/$repo/os/$arch/$filename"
 						fi
 						error=1
 					fi
@@ -259,54 +343,60 @@ consistencycheck()
 	return $?
 }
 
+setConfiguration "$@"
 
+if [ "$CONFIG_ACTION" = "usage" ]
+then
+	usage
+	exit 0
+fi
 
-if ! [ $ONLY_CHECK_CONSISTENCY -eq 0 ]
+if [ "$CONFIG_ACTION" = "check" ]
 then
 	log 1 "*** only checking consistency: $(date)"
 else
 	log 1 "*** synchronization started: $(date)"
-	log 1 \(II\) using mirror $mirror
-	log 1 \(II\) using $paralleldownloads parallel rsync instances
-	log 1 \(II\) repos: $repos
-	log 1 \(II\) archs: $archs
+	log 1 \(II\) using mirror $CONFIG_MIRROR
+	log 1 \(II\) using $CONFIG_PARALLELDOWNLOADS parallel rsync instances
+	log 1 \(II\) repos: $CONFIG_REPOS
+	log 1 \(II\) archs: $CONFIG_ARCHS
 fi
 
 rsyncopts="-abv --copy-unsafe-links --no-motd --delete --ignore-errors --backup-dir=backup"
 
 dcount=0
-for repo in $repos
+for repo in $CONFIG_REPOS
 do
 	ierules=""
 
-	for arch in $archs
+	for arch in $CONFIG_ARCHS
 	do
 		if ( [ "$repo" = "multilib" ] || [ "$repo" = "multilib-testing" ] ) && [ "$arch" != "x86_64" ]
 		then
 			continue
 		fi
 
-		mkdir -p $repo/os/$arch
+		mkdir -p "$CONFIG_TARGETDIR/$repo/os/$arch"
 		ierules="$ierules --include="$arch" --include="$arch/**""
 	done
 	ierules="$ierules --exclude="*""
 
 	(
-		if [ $ONLY_CHECK_CONSISTENCY -eq 0 ]
+		if [ "$CONFIG_ACTION" = "sync" ]
 		then
 			log 1 \(II\) syncing $repo/os ...
 
-			rsync $rsyncopts $ierules $mirror/$repo/os/ $repo/os/ 2>> sync.log 1> /dev/null
+			rsync $rsyncopts $ierules "$CONFIG_MIRROR/$repo/os/" "$CONFIG_TARGETDIR/$repo/os/" 2>> "$CONFIG_TARGETDIR/sync.log" 1> /dev/null
 
-			for arch in $archs
+			for arch in $CONFIG_ARCHS
 			do
 				if ! [ "$arch" = "any" ]
 				then
 					for db in abs db files
 					do
-						if [ -e $repo/os/backup/$arch/$repo.$db.tar.gz.consistent ]
+						if [ -e "$CONFIG_TARGETDIR/$repo/os/backup/$arch/$repo.$db.tar.gz.consistent" ]
 						then
-							mv $repo/os/backup/$arch/$repo.$db.tar.gz.consistent $repo/os/$arch/
+							mv "$CONFIG_TARGETDIR/$repo/os/backup/$arch/$repo.$db.tar.gz.consistent" "$CONFIG_TARGETDIR/$repo/os/$arch/"
 						fi
 					done
 				fi
@@ -317,27 +407,27 @@ do
 	) &
 	dcount=$(( $dcount + 1))
 
-	if [ $dcount -ge $paralleldownloads ]
+	if [ $dcount -ge $CONFIG_PARALLELDOWNLOADS ]
 	then
 		wait
 		dcount=0
 	fi
 done
 
-if [ $ONLY_CHECK_CONSISTENCY -eq 0 ]
+if [ "$CONFIG_ACTION" = "sync" ]
 then
 	wait
 
 	ierules="--exclude="backup""
 	
-	mkdir -p iso/latest
+	mkdir -p "$CONFIG_TARGETDIR/iso/latest"
 
 	log 1 \(II\) syncing iso images...	
-	rsync $rsyncopts $ierules $mirror/iso/latest/ iso/latest/ 2>> sync.log 1> /dev/null
+	rsync $rsyncopts $ierules "$CONFIG_MIRROR/iso/latest/" "$CONFIG_TARGETDIR/iso/latest/" 2>> "$CONFIG_TARGETDIR/sync.log" 1> /dev/null
 	log 1 \(II\) done syncing iso images.
 
 	log 1 \(II\) cleaning up iso images...
-	rm -rf iso/latest/backup
+	rm -rf "$CONFIG_TARGETDIR/iso/latest/backup"
 
 	log 1 \(II\) done cleaning up iso images.
 fi
@@ -346,9 +436,9 @@ wait
 
 error=0
 errorold=0
-for repo in $repos
+for repo in $CONFIG_REPOS
 do
-	for arch in $archs
+	for arch in $CONFIG_ARCHS
 	do
                 if ( [ "$repo" = "multilib" ] || [ "$repo" = "multilib-testing" ] ) && [ "$arch" != "x86_64" ]
                 then
@@ -362,51 +452,51 @@ do
 
 		log 1 \(II\) checking consistency of $repo/os/$arch ...
 
-		if ! consistencycheck $repo $arch
+		if ! consistencycheck "$CONFIG_TARGETDIR" $repo $arch $CONFIG_INTEGRITY_CHECK
 		then
 			errlog \(WW\) $repo/os/$arch is inconsistent... trying to revert to old db...
 
 			for db in abs db files
 			do
-				if [ -e $repo/os/$arch/$repo.$db.tar.gz.consistent ]
+				if [ -e "$CONFIG_TARGETDIR/$repo/os/$arch/$repo.$db.tar.gz.consistent" ]
 				then	
-					cp $repo/os/$arch/$repo.$db.tar.gz.consistent $repo/os/$arch/$repo.$db.tar.gz
+					cp "$CONFIG_TARGETDIR/$repo/os/$arch/$repo.$db.tar.gz.consistent" "$CONFIG_TARGETDIR/$repo/os/$arch/$repo.$db.tar.gz"
 				fi
 			done
 
-			cp -r $repo/os/backup/$arch/. $repo/os/$arch/
-			cp -r $repo/os/backup/any/. $repo/os/any/
+			cp -r "$CONFIG_TARGETDIR/$repo/os/backup/$arch/." "$CONFIG_TARGETDIR/$repo/os/$arch/"
+			cp -r "$CONFIG_TARGETDIR/$repo/os/backup/any/." "$CONFIG_TARGETDIR/$repo/os/any/"
 
 
-			if ! consistencycheck $repo $arch
+			if ! consistencycheck "$CONFIG_TARGETDIR" $repo $arch $CONFIG_INTEGRITY_CHECK
 			then
 				errlog \(EE\) reverting $repo/os/$arch to a consistent state failed
 				error=1
 			else
 				log 1 \(II\) reverting was successful
 			fi
-		elif [ $ONLY_CHECK_CONSISTENCY -eq 0 ]
+		elif [ "$CONFIG_ACTION" = "sync" ]
 		then
 			log 1 \(II\) $repo/os/$arch seems to be consistent... cleaning up $repo/os/$arch ...
 
-			rm -rf $repo/os/backup/$arch
+			rm -rf "$CONFIG_TARGETDIR/$repo/os/backup/$arch"
 
 			log 1 \(II\) done cleaning up $repo/os/$arch.
 
 			for db in abs db files
 			do
-				if [ -e $repo/os/$arch/$repo.$db.tar.gz ]
+				if [ -e "$CONFIG_TARGETDIR/$repo/os/$arch/$repo.$db.tar.gz" ]
 				then
-					cp $repo/os/$arch/$repo.$db.tar.gz $repo/os/$arch/$repo.$db.tar.gz.consistent
+					cp "$CONFIG_TARGETDIR/$repo/os/$arch/$repo.$db.tar.gz" "$CONFIG_TARGETDIR/$repo/os/$arch/$repo.$db.tar.gz.consistent"
 				fi
 			done
 		fi
 	done
 
-	if [ $error -eq 0 ] && [ $ONLY_CHECK_CONSISTENCY -eq 0 ]
+	if [ $error -eq 0 ] && [ "$CONFIG_ACTION" = "sync" ]
 	then
 		log 1 \(II\) $repo/os/\* seems to be consistent... cleaning up $repo/os ...
-		rm -rf $repo/os/backup
+		rm -rf "$CONFIG_TARGETDIR/$repo/os/backup"
 	fi
 done
 
